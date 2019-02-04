b34191dc7976d71658e64dd958b37c2b4f1ed7d7
halynamecherzhak <37458276+halynamecherzhak@users.noreply.github.com>
2019-02-04 10:58:32 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -19,12 +19,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         body{ font: 14px sans-serif; text-align: center; }
     </style>
 </head>
+
 <body>
 <div class="page-header">
-    <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b></h1>
 </div>
 <p>
-    <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
     <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
 </p>
 </body>
